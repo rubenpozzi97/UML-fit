d742228ab964bfa53c297decfa984353df1ca13d
Alessio Boletti <boletti@pd.infn.it>
2020-09-10 13:02:52 +0200

    reduce submission to 20 toys

--- sub_simfit_recoMC_fullAngular.sh
+++ sub_simfit_recoMC_fullAngular.sh
@@ -10,15 +10,15 @@ nsamp       = ( \$(ProcId) / 6 ) + 1
 bin         = \$(ProcId) % 6
 Arguments   = \$INT(nsamp) \$INT(bin)
 Log         = logs_parSub/sub_\$(ClusterId).log
-Output      = logs_parSub/simfit_recoMC_fullAngular_\$INT(nsamp)_\$INT(bin).out
-Error       = logs_parSub/simfit_recoMC_fullAngular_\$INT(nsamp)_\$INT(bin).err
+Output      = logs_parSub/simfit_recoMC_fullAngular_RefTest_\$INT(nsamp)_\$INT(bin).out
+Error       = logs_parSub/simfit_recoMC_fullAngular_RefTest_\$INT(nsamp)_\$INT(bin).err
 +JobFlavour = "testmatch"
 EOF
 
 if [ "${USER}" == "fiorendi" ]; then
     echo '+AccountingGroup = "group_u_CMST3.all"'>>temp_sub_simfit_recoMC_fullAngular_oneBin.sub
 fi
-echo 'Queue 600'>>temp_sub_simfit_recoMC_fullAngular_oneBin.sub
+echo 'Queue 120'>>temp_sub_simfit_recoMC_fullAngular_oneBin.sub
 
 # Compilation, submission and file removal
 if make simfit_recoMC_fullAngular
