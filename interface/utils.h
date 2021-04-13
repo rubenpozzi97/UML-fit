ca161266a8c175907a32708eee3afa106976477d
Maria Faria <carolcarolina98@hotmail.com>
2021-04-13 19:40:26 +0200

    validation

--- interface/utils.h
+++ interface/utils.h
@@ -1,6 +1,7 @@
 #include <RooGaussian.h>
 #include <RooPlot.h>
 #include <RooRealVar.h>
+#include <RooAbsReal.h>
 #include <RooWorkspace.h>
 #include <RooConstVar.h>
 #include <map>
@@ -62,7 +63,7 @@ RooGaussian* constrainVar(RooRealVar* var,
     return gauss_constr;                 
 }
 
-void constrainVar2(RooRealVar* var, 
+void constrainVar2(RooAbsReal* var, 
                    string inVarName,  
                    RooWorkspace *w, 
                    int year,
@@ -82,6 +83,31 @@ void constrainVar2(RooRealVar* var,
     }
 }
 
+void constrainVar3(TString input_file,
+		   int year,
+		   int q2Bin,
+                   bool addToList,
+                   RooArgSet &c_vars,
+		   RooArgSet &c_pdfs
+                   ){
+
+  TFile* f = TFile::Open(input_file);
+  RooFitResult* fitresult = (RooFitResult*)f->Get(Form("simFitResult_b%ip2c1subs0",q2Bin));
+
+  RooRealVar* mean_difference = (RooRealVar*)fitresult->floatParsFinal().find(Form("mean_difference^{%i}",year));
+
+  RooGaussian* gauss_constr = new RooGaussian(Form("c_%s_%i", mean_difference->GetName(), year) ,
+                                              Form("c_%s_%i", mean_difference->GetName(), year),
+                                              *mean_difference,
+                                              RooConst(mean_difference->getVal()),
+                                              RooConst(mean_difference->getError())
+                                              );
+
+    if (addToList){
+      c_vars.add(*mean_difference);
+      c_pdfs.add(*gauss_constr);
+    }
+}
 
 bool retrieveWorkspace(string filename, std::vector<RooWorkspace*> &ws, std::string ws_name, std::vector<RooWorkspace*> &ws1, std::string ws_name_odd, int parity){
 
@@ -109,8 +135,9 @@ bool retrieveWorkspace(string filename, std::vector<RooWorkspace*> &ws, std::str
 
 std::vector<RooDataSet*> createDataset(int nSample, uint firstSample, uint lastSample, RooWorkspace *ws, RooWorkspace *ws1, 
                                        int q2Bin, int parity, int year, //std::map<int,float> scale_to_data,
-                                       RooArgSet reco_vars, std::string shortString, int comp){
+                                       RooArgSet reco_vars, std::string shortString, int comp, int dat){
 
+    RooDataSet* data;
     RooDataSet* dataCT, *dataWT;
     RooDataSet* dataCT_even, *dataWT_even;
     RooDataSet* dataCT_odd, *dataWT_odd;
@@ -178,90 +205,117 @@ std::vector<RooDataSet*> createDataset(int nSample, uint firstSample, uint lastS
 					   ("data_"+shortString + "_subs0").c_str(), 
 					   RooArgSet(reco_vars));
 
-      if(parity < 2){
-        dataCT = (RooDataSet*)ws->data(Form((parity==1?"data_ctRECO_ev_b%i":"data_ctRECO_od_b%i"),q2Bin)) ;
-        dataWT = (RooDataSet*)ws->data(Form((parity==1?"data_wtRECO_ev_b%i":"data_wtRECO_od_b%i"),q2Bin)) ;
-      }
-      else{
-        dataCT_even = (RooDataSet*)ws->data(Form(("data_ctRECO_ev_b%i"),q2Bin));
-        dataCT_odd = (RooDataSet*)ws1->data(Form(("data_ctRECO_od_b%i"),q2Bin));
-        dataWT_even = (RooDataSet*)ws->data(Form(("data_wtRECO_ev_b%i"),q2Bin));
-        dataWT_odd = (RooDataSet*)ws1->data(Form(("data_wtRECO_od_b%i"),q2Bin));
+      if(dat == 0){
+        if(parity < 2){
+          dataCT = (RooDataSet*)ws->data(Form((parity==1?"data_ctRECO_ev_b%i":"data_ctRECO_od_b%i"),q2Bin)) ;
+          dataWT = (RooDataSet*)ws->data(Form((parity==1?"data_wtRECO_ev_b%i":"data_wtRECO_od_b%i"),q2Bin)) ;
+        }
+        else{
+          dataCT_even = (RooDataSet*)ws->data(Form(("data_ctRECO_ev_b%i"),q2Bin));
+          dataCT_odd = (RooDataSet*)ws1->data(Form(("data_ctRECO_od_b%i"),q2Bin));
+          dataWT_even = (RooDataSet*)ws->data(Form(("data_wtRECO_ev_b%i"),q2Bin));
+          dataWT_odd = (RooDataSet*)ws1->data(Form(("data_wtRECO_od_b%i"),q2Bin));
+        }
+
+        if(comp == 0){
+          if(parity < 2){isample->append(*dataCT);}
+          else{
+            isample->append(*dataCT_even);
+            isample->append(*dataCT_odd);
+          }
+         }
+        else if(comp == 1){
+          if(parity < 2){isample->append(*dataWT);}
+          else{
+            isample->append(*dataWT_even);
+            isample->append(*dataWT_odd);
+          }
+        }
+        else{
+          if(parity < 2){
+            isample->append(*dataCT);
+            isample->append(*dataWT);
+          }
+          else{
+            isample->append(*dataCT_even);
+            isample->append(*dataCT_odd);
+            isample->append(*dataWT_even);
+            isample->append(*dataWT_odd);
+          }
+        }
       }
 
-      if(comp == 0){
-        if(parity < 2){isample->append(*dataCT);}
-        else{
-          isample->append(*dataCT_even);
-          isample->append(*dataCT_odd);
-        }
+      else if(dat == 1){ 
+        data = (RooDataSet*)ws->data(Form("Dataset/data_b%i",q2Bin));
+        isample->append(*data);
       }
-      else if(comp == 1){
-        if(parity < 2){isample->append(*dataWT);}
-        else{
-          isample->append(*dataWT_even);
-          isample->append(*dataWT_odd);
-        }
-      }
-      else{
-        if(parity < 2){
-          isample->append(*dataCT);
-          isample->append(*dataWT);
-        }
-        else{
-          isample->append(*dataCT_even);
-          isample->append(*dataCT_odd);
-          isample->append(*dataWT_even);
-          isample->append(*dataWT_odd);
-        }
-      }
+
       datasample.push_back (isample); 
     }
  
     return datasample;
 }
 
-void validate_fit(RooWorkspace* w, RooCategory sample, int year, int q2Bin, int parity){
+void validate_fit(RooWorkspace* w, RooCategory sample, RooArgSet c_vars, int year, int q2Bin, int parity){
 
   RooRealVar mass = *(w->var("mass"));
-
   RooAbsPdf* model = w->pdf("simPdf");
 
   vector<RooRealVar> params;
   params.push_back(*(w->var( Form("sig_yield^{%i}",year) )));
-  string var_name = (w->var( Form("sig_yield^{%i}",year)) )->GetName();
+  params.push_back(*(w->var( Form("mFrac^{%i}",year) )));
+  params.push_back(*(w->var( Form("mean_{RT}^{%i}",year) )));
+  params.push_back(*(w->var( Form("#sigma_{RT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("#alpha_{RT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("#alpha_{RT2}^{%i}",year) )));
+  params.push_back(*(w->var( Form("n_{RT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("n_{RT2}^{%i}",year) )));
+  if(q2Bin >= 5){
+    params.push_back(*(w->var( Form("#sigma_{RT2}^{%i}",year) )));
+    params.push_back(*(w->var( Form("f^{RT%i}",year) )));
+  }
+  params.push_back(*(w->var( Form("mean_difference^{%i}",year) )));
+  params.push_back(*(w->var( Form("#sigma_{WT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("#alpha_{WT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("#alpha_{WT2}^{%i}",year) )));
+  params.push_back(*(w->var( Form("n_{WT1}^{%i}",year) )));
+  params.push_back(*(w->var( Form("n_{WT2}^{%i}",year) )));
 
+  string var_name;
   int params_size = params.size();
-  cout << "params size = " << params_size << endl;
-  cout << "variable value = " << params[0].getVal() << endl;
 
   cout << "Starting toy MC study" << endl;
-  RooMCStudy* mcstudy = new RooMCStudy(*model, RooArgSet(mass,sample), Binned(kTRUE), Silence(kTRUE), Extended(), FitOptions(Save(kTRUE), PrintEvalErrors(0)));
-  mcstudy->generateAndFit(5000);
+  RooMCStudy* mcstudy = new RooMCStudy(*model, RooArgSet(mass,sample), Constrain(c_vars), Binned(kFALSE), Silence(kTRUE), Extended(), FitOptions(Save(kTRUE), PrintEvalErrors(0)));
+  mcstudy->generateAndFit(1000);
   cout << "Ending toy MC study" << endl;
 
   vector<RooPlot*> framesPull, framesParam;
 
   for(int i = 0; i < params_size; ++i){
-    framesPull.push_back(mcstudy->plotPull(params.at(i),FrameBins(200)));//FrameRange(-5,5)
+    framesPull.push_back(mcstudy->plotPull(params.at(i),FrameBins(100)));//200
     framesPull[i]->SetTitle("");
     framesParam.push_back(mcstudy->plotParam(params.at(i),FrameBins(50)));
     framesParam[i]->SetTitle("");
   }
-
+  
   vector<TGraph*> h;
-
   for(int i = 0; i < params_size; ++i){
     h.push_back(static_cast<TGraph*>(framesPull.at(i)->getObject(0)));
   }
 
   gStyle->SetOptFit(0111);
 
-  TCanvas* c_pull = new TCanvas("pulls", "pulls", 900, 800);
-
+  TCanvas* c_pull;
   gPad->SetLeftMargin(0.15);
 
+  ofstream output(Form("simFitPullResults/out_b%i_year_%i.dat", q2Bin, year));
+  output << '|' << setw(15) <<  "var_name" << '|' << setw(15) <<  "  pull mean  " << '|' << setw(15) <<  "  pull width  " << endl;
+
   for(int i = 0; i < params_size; ++i){
+    c_pull = new TCanvas( "pulls", "pulls", 900, 800);
+    c_pull->cd();
+    var_name = params[i].GetName();
+
     h[i]->SetTitle("");
     h[i]->Draw();
     c_pull->Update();
@@ -273,18 +327,27 @@ void validate_fit(RooWorkspace* w, RooCategory sample, int year, int q2Bin, int 
     h[i]->GetYaxis()->SetTitle("Toy MCs");
     h[i]->GetXaxis()->SetRangeUser(-10,10);
     h[i]->Draw("same");
+
+    TF1* fit = h[i]->GetFunction("gaus");
+    
+    output << '|' << setw(15) <<  var_name << '|' << setw(15) <<  fit->GetParameter(1) << '|' << setw(15) <<  fit->GetParameter(2) << endl;
+    
+    c_pull->SaveAs(( "plotSimMassFit_pulls/pulls_poisson_" + var_name + Form("b%ip%i_%i",q2Bin,parity,year) + ".gif").c_str() );
+    delete c_pull;
   }
+  output.close();
 
   TCanvas* c_params = new TCanvas("params", "params", 900, 800);
 
   for(int i = 0; i < params_size; ++i){
+    var_name = params[i].GetName();
+
     c_params->cd();
     framesParam.at(i)->GetYaxis()->SetTitleOffset(1.4);
     framesParam.at(i)->Draw();
-  }
 
-  c_pull->SaveAs( ("plotSimMassFit_pulls/pulls_poisson_" + var_name + Form("b%ip%i_%i",q2Bin,parity,year) + ".gif").c_str() );
-  c_params->SaveAs( ("plotSimMassFit_pulls/pulls_params_poisson_" + var_name + Form("b%ip%i_%i",q2Bin,parity,year) + ".gif").c_str() );
+    c_params->SaveAs(( "plotSimMassFit_pulls/pulls_params_poisson_" + var_name + Form("b%ip%i_%i",q2Bin,parity,year) + ".gif").c_str() );
+  }
 
 }
 
