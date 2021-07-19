87cdb4fbc8554dc4247d97c996649415645ebb5d
Maria Faria <carolcarolina98@hotmail.com>
2021-07-19 17:00:45 +0200

    update

--- interface/PdfSigRTMass.h
+++ interface/PdfSigRTMass.h
@@ -1,19 +1,20 @@
 #include <RooAddPdf.h>
 #include <RooCBShape.h>
+#include <RooGaussian.h>
 #include "RooDoubleCBFast.h"
+#include <RooAbsReal.h>
 #include <RooRealVar.h>
 #include <RooWorkspace.h>
-#include <RooArgSet.h>
-
+ 
 RooDoubleCBFast* createRTMassShape(int q2Bin,
-                                   RooRealVar* x,
-                                   RooRealVar* mean_rt,
-                                   RooRealVar* sigma_rt,
-                                   RooRealVar* alpha_rt1,
-                                   RooRealVar* alpha_rt2,
-                                   RooRealVar* n_rt1,
-                                   RooRealVar* n_rt2,
-                                   RooWorkspace *w,
+                                   RooAbsReal* x,
+                                   RooAbsReal* mean_rt,
+                                   RooAbsReal* sigma_rt,
+                                   RooAbsReal* alpha_rt1,
+                                   RooAbsReal* alpha_rt2,
+                                   RooAbsReal* n_rt1,
+                                   RooAbsReal* n_rt2,
+                                   TString input_file,
                                    int year,
                                    bool constrainVars, 
                                    RooArgSet &c_vars,
@@ -27,34 +28,32 @@ RooDoubleCBFast* createRTMassShape(int q2Bin,
                                                    );
     
     if (constrainVars){
-        constrainVar2(sigma_rt , Form("#sigma_{RT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_rt1, Form("#alpha_{RT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_rt2, Form("#alpha_{RT2}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_rt1    , Form("n_{RT1}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_rt2    , Form("n_{RT2}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#sigma_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#alpha_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#alpha_{RT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("n_{RT1}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("n_{RT2}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
     }
     
     return dcb_rt;                                                   
 }
 
-
-
-RooAddPdf* createRTMassShape(int q2Bin,
-                             RooRealVar* x,
-                             RooRealVar* mean_rt,
-                             RooRealVar* sigma_rt,
-                             RooRealVar* sigma_rt2,
-                             RooRealVar* alpha_rt1,
-                             RooRealVar* alpha_rt2,
-                             RooRealVar* n_rt1,
-                             RooRealVar* n_rt2,
-                             RooRealVar* f1rt,
-                             RooWorkspace *w,
-                             int year,
-                             bool constrainVars, 
-                             RooArgSet &c_vars,
-                             RooArgSet &c_pdfs
-                             ){
+RooAddPdf* createRTMassShape2(int q2Bin,
+                              RooAbsReal* x,
+                              RooAbsReal* mean_rt,
+                              RooAbsReal* sigma_rt,
+                              RooAbsReal* sigma_rt2,
+                              RooAbsReal* alpha_rt1,
+                              RooAbsReal* alpha_rt2,
+                              RooAbsReal* n_rt1,
+                              RooAbsReal* n_rt2,
+                              RooAbsReal* f1rt,
+                              TString input_file,
+                              int year,
+                              bool constrainVars, 
+                              RooArgSet &c_vars,
+                              RooArgSet &c_pdfs
+                              ){
 
     RooCBShape* cbshape_rt1 = new RooCBShape (Form("cbshape_rt1_%i", year), 
                                               Form("cbshape_rt1_%i", year),  
@@ -67,37 +66,101 @@ RooAddPdf* createRTMassShape(int q2Bin,
                                               *mean_rt, *sigma_rt2, *alpha_rt2, *n_rt2
                                               );
 
-    double integral1 = ( cbshape_rt1->createIntegral(RooArgSet(*x)) )->getVal();
-    double integral2 = ( cbshape_rt2->createIntegral(RooArgSet(*x)) )->getVal();
-
-    RooRealVar* int_1 = new RooRealVar("int_1", "int_1", 1.0/integral1);
-    RooRealVar* int_2 = new RooRealVar("int_2", "int_2", 1.0/integral2);
-    int_1->setConstant();
-    int_2->setConstant();
-
-    RooAddPdf* cbshape_rt1_normalised = new RooAddPdf(Form("cbshape_rt1_%i_normalised", year),
-						      Form("cbshape_rt1_%i_normalised", year),
-						      *cbshape_rt1, *int_1
-						      );
-    RooAddPdf* cbshape_rt2_normalised = new RooAddPdf(Form("cbshape_rt2_%i_normalised", year),
-                                                      Form("cbshape_rt2_%i_normalised", year),
-                                                      *cbshape_rt2, *int_2
-                                                      );
-
     RooAddPdf* dcb_rt = new RooAddPdf (Form("dcb_rt_%i", year) , 
                                        Form("dcb_rt_%i", year) ,  
-                                       RooArgList(*cbshape_rt1_normalised,*cbshape_rt2_normalised), 
+                                       RooArgList(*cbshape_rt1,*cbshape_rt2), 
                                        RooArgList(*f1rt));
 
-    if (constrainVars){
-        constrainVar2(sigma_rt , Form("#sigma_{RT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_rt1, Form("#alpha_{RT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_rt2, Form("#alpha_{RT2}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_rt1    , Form("n_{RT1}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_rt2    , Form("n_{RT2}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
-        //constrainVar2(sigma_rt2 , Form("#sigma_{RT2}^{%i}",q2Bin), w, year, true, c_vars, c_pdfs);
-        //constrainVar2(f1rt      , Form("f^{RT%i}"         ,q2Bin), w, year, true, c_vars, c_pdfs);
+    if (constrainVars){ 
+        constrainVar3(input_file, Form("#sigma_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#alpha_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#alpha_{RT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("n_{RT1}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("n_{RT2}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#sigma_{RT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("f^{RT%i}"         ,year) , year, q2Bin, true, c_vars, c_pdfs);
     }
     
     return dcb_rt;                                                   
 }
+
+RooAddPdf* createRTMassShape3(int q2Bin,
+                              RooAbsReal* x,
+                              RooAbsReal* mean_rt,
+                              RooAbsReal* sigma_rt,
+                              RooAbsReal* sigma_rt2,
+                              RooAbsReal* alpha_rt1,
+                              RooAbsReal* n_rt1,
+                              RooAbsReal* f1rt,
+                              TString input_file,
+                              int year,
+                              bool constrainVars,
+                              RooArgSet &c_vars,
+                              RooArgSet &c_pdfs
+                              ){
+
+    RooCBShape* cbshape_rt = new RooCBShape (Form("cbshape_rt_%i", year),
+                                              Form("cbshape_rt_%i", year),
+                                              *x,
+                                              *mean_rt, *sigma_rt , *alpha_rt1, *n_rt1
+                                              );
+    RooGaussian* gaussian_rt = new RooGaussian (Form("gaussian_rt_%i", year),  
+                                                Form("gaussian_rt_%i", year),  
+                                                *x,  
+                                                *mean_rt, *sigma_rt2
+                                                );
+
+    RooAddPdf* dcb_rt = new RooAddPdf (Form("dcb_rt_%i", year) ,
+                                       Form("dcb_rt_%i", year) ,
+                                       RooArgList(*cbshape_rt,*gaussian_rt),
+                                       RooArgList(*f1rt));
+
+    if (constrainVars){
+        constrainVar3(input_file, Form("#sigma_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#alpha_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("n_{RT1}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#sigma_{RT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("f^{RT%i}"         ,year) , year, q2Bin, true, c_vars, c_pdfs);
+    }
+
+    return dcb_rt;
+}
+
+RooAddPdf* createRTMassShape4(int q2Bin,
+                              RooAbsReal* x,
+                              RooAbsReal* mean_rt,
+                              RooAbsReal* sigma_rt,
+                              RooAbsReal* sigma_rt2,
+                              RooAbsReal* f1rt,
+                              TString input_file,
+                              int year,
+                              bool constrainVars,
+                              RooArgSet &c_vars,
+                              RooArgSet &c_pdfs
+                              ){
+
+    RooGaussian* gaussian_rt1 = new RooGaussian (Form("gaussian_rt1_%i", year),
+                                                 Form("gaussian_rt1_%i", year),
+                                                 *x,
+                                                 *mean_rt, *sigma_rt 
+                                                 );
+    RooGaussian* gaussian_rt2 = new RooGaussian (Form("gaussian_rt2_%i", year),
+                                                 Form("gaussian_rt2_%i", year),
+                                                 *x,
+                                                 *mean_rt, *sigma_rt2
+                                                 );
+
+    RooAddPdf* dcb_rt = new RooAddPdf (Form("dcb_rt_%i", year) ,
+                                       Form("dcb_rt_%i", year) ,
+                                       RooArgList(*gaussian_rt1,*gaussian_rt2),
+                                       RooArgList(*f1rt));
+
+    if (constrainVars){
+        constrainVar3(input_file, Form("#sigma_{RT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("#sigma_{RT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        constrainVar3(input_file, Form("f^{RT%i}"         ,year) , year, q2Bin, true, c_vars, c_pdfs);
+    }
+
+    return dcb_rt;
+}
+
--- interface/PdfSigWTMass.h
+++ interface/PdfSigWTMass.h
@@ -1,37 +1,37 @@
 #include "RooDoubleCBFast.h"
+#include <RooAbsReal.h>
 #include <RooRealVar.h>
 #include <RooWorkspace.h>
 
-
 RooDoubleCBFast* createWTMassShape(int q2Bin,
-                                   RooRealVar* x,
-                                   RooRealVar* mean_wt,
-                                   RooRealVar* sigma_wt,
-                                   RooRealVar* alpha_wt1,
-                                   RooRealVar* alpha_wt2,
-                                   RooRealVar* n_wt1,
-                                   RooRealVar* n_wt2,
-                                   RooWorkspace *w,
+                                   RooAbsReal* x,
+                                   RooAbsReal* mean_wt,
+                                   RooAbsReal* sigma_wt,
+                                   RooAbsReal* alpha_wt1,
+                                   RooAbsReal* alpha_wt2,
+                                   RooAbsReal* n_wt1,
+                                   RooAbsReal* n_wt2,
+                                   TString input_file,
                                    int year,
                                    bool constrainVars, 
                                    RooArgSet &c_vars,
                                    RooArgSet &c_pdfs
                                    ){
 
-    RooDoubleCBFast* dcb_wt = new RooDoubleCBFast ( Form("dcb_wt_%i", year)  , 
-                                                   "dcb_wt"      , 
+    RooDoubleCBFast* dcb_wt = new RooDoubleCBFast (Form("dcb_wt_%i", year), 
+                                                   "dcb_wt", 
                                                    *x, 
                                                    *mean_wt, *sigma_wt, *alpha_wt1, *n_wt1, *alpha_wt2, *n_wt2
                                                    );
-
+   
     if (constrainVars){
-        constrainVar2(sigma_wt , Form("#sigma_{WT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_wt1, Form("#alpha_{WT1}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(alpha_wt2, Form("#alpha_{WT2}^{%i}",q2Bin) , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_wt1    , Form("n_{WT1}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
-        constrainVar2(n_wt2    , Form("n_{WT2}^{%i}",q2Bin)      , w, year, true, c_vars, c_pdfs);
+        //constrainVar3(input_file, Form("#sigma_{WT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        //constrainVar3(input_file, Form("#alpha_{WT1}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        //constrainVar3(input_file, Form("#alpha_{WT2}^{%i}",year) , year, q2Bin, true, c_vars, c_pdfs);
+        //constrainVar3(input_file, Form("n_{WT1}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs);
+        //constrainVar3(input_file, Form("n_{WT2}^{%i}",year)      , year, q2Bin, true, c_vars, c_pdfs); 
     }
-
+    
     return dcb_wt;                                                   
 }
 
